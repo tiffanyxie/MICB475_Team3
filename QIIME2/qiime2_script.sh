b084f6330be9fc46bbe5ae16c3807df1b32592eb
ivanadjaja <ivana.djaja@outlook.com>
2026-02-18 00:00:46 -0500

    Update qiime2_script.sh

--- QIIME2/qiime2_script.sh
+++ QIIME2/qiime2_script.sh
@@ -87,3 +87,47 @@ qiime feature-table summarize \
   --i-table table-no-mitochondria-no-chloroplast.qza \
   --o-visualization table-no-mitochondria-no-chloroplast.qzv \
   --m-sample-metadata-file soil_metadata.txt
+
+# Select for organic layer (o-horizon) and no herbicide treated (0) samples
+qiime feature-table filter-samples \
+  --i-table table-no-mitochondria-no-chloroplast.qza \
+  --m-metadata-file /datasets/project_2/soil/soil_metadata.txt \
+  --p-where "[Horizon]='O horizon' AND [Herbicide Use]='0'" \
+  --o-filtered-table o-layer-no-herbicide-filtered-table.qza
+
+# Further filter for BC only table
+  qiime feature-table filter-samples \
+  --i-table o-layer-no-herbicide-filtered-table.qza \
+  --m-metadata-file /datasets/project_2/soil/soil_metadata.txt \
+  --p-where "[Region]='British Columbia'" \
+  --o-filtered-table bc-only-o-layer-no-herbicide-filtered-table.qza
+
+qiime feature-table summarize \
+  --i-table bc-only-o-layer-no-herbicide-filtered-table.qza \
+  --o-visualization bc-only-o-layer-no-herbicide-filtered-table.qzv \
+  --m-sample-metadata-file /datasets/project_2/soil/soil_metadata.txt
+
+# Generate tree for phylogenetic diversity analyses
+qiime phylogeny align-to-tree-mafft-fasttree \
+--i-sequences rep-seqs.qza \
+--o-alignment aligned-rep-seqs.qza \
+--o-masked-alignment masked-aligned-rep-seqs.qza \
+--o-tree unrooted-tree.qza \
+--o-rooted-tree rooted-tree.qza
+
+# only run until here (ivana) -----
+
+# Alpha rarefaction curve (8000 is close to 8747)
+qiime diversity alpha-rarefaction \
+--i-table table.qza \
+--i-phylogeny rooted-tree.qza \
+--p-max-depth 8000 \
+--m-metadata-file /datasets/project_2/soil/soil_metadata.txt \
+--o-visualization alpha-rarefaction.qzv
+
+qiime diversity core-metrics-phylogenetic \
+--i-phylogeny rooted-tree.qza \
+--i-table table.qza \
+--p-sampling-depth 5296 \
+--m-metadata-file /datasets/project_2/soil/soil_metadata.txt \
+--output-dir core-metrics-results
