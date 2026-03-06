414126570c3627519748015511c373faa6d68a6a
ColeSternHarrison <coleharrison0107@gmail.com>
2026-03-06 10:22:53 -0800

    Update and rename 1_Diversity_Metrics_A.R to 1_Phyloseq_object.R
    Dedicated this R script to generation of the phyloseq object. Removed code used for diversity metrics, as this is found in the 1_Diversity_by_metadata.R file

---
+++ R_scripts/1_Phyloseq_object.R
@@ -0,0 +1,43 @@
+library(phyloseq)
+library(ape) # importing trees
+library(tidyverse)
+library(picante)
+
+#### Load Data ####
+otu <- read_delim(file = "feature-table.txt", delim = "\t", skip=1)
+sampdat <- read_delim(file = "soil_metadata.txt", delim = "\t")
+taxonomy <- read_delim(file = "taxonomy.tsv", delim = "\t")
+phylotree <- read.tree("tree.nwk")
+
+
+#### Preparing Data to Generate Phyloseq Object ####
+#Format OTU table
+otu_mat <- as.matrix(otu[,-1])
+rownames(otu_mat) <- otu$`#OTU ID`
+OTU <- otu_table(otu_mat, taxa_are_rows = TRUE)
+
+#Format Sample Metadata
+samp_df <- as.data.frame(sampdat[,-1])
+rownames(samp_df) <- sampdat$`#SampleID`
+SAMP <- sample_data(samp_df)
+
+#Format taxonomy table
+tax_mat <- taxonomy %>%
+  select(-Confidence) %>%
+  separate(col=Taxon, sep = ";",
+           into = c("Domain","Phylum","Class","Order","Family","Genus","Species")) %>%
+  as.matrix()
+tax_mat <- tax_mat[,-1]
+rownames(tax_mat) <- taxonomy$`Feature ID`
+TAX <- tax_table(tax_mat)
+
+#### Create phyloseq object####
+phylo_soil <- phyloseq(OTU, SAMP, TAX, phylotree)
+#Rarefy to a depth of 3310
+phylo_soil_rare <- rarefy_even_depth(phylo_soil, rngseed = 67, sample.size = 3310)
+
+save(phylo_soil, file = "phylo_soil.RData")
+save(phylo_soil_rare, file = "phylo_soil_rare.RData")
+
+load("phylo_soil.RData")
+load("phylo_soil_rare.RData")
